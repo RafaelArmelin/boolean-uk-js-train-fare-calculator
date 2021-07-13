f2e176cfff40aac6da0e968a672223ec9817a3d0
Rafael Armelin <raffael.armelin@gmail.com>
2021-07-13 16:11:44 +0100

    finished

--- index.js
+++
@@ -1,7 +0,0 @@
-
-// Instructions
-// - Ask the user for their age and how far they are travelling (in km)
-// - The price per travelled km will be £0.21
-// - Junior passengers (under 18) get a 20% discount
-// - Senior passengers (over 65) get a 40% discount
-
