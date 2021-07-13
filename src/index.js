1b6a5948135618f2e1c83243a524dc8fc5e6ecb9
Rafael Armelin <raffael.armelin@gmail.com>
2021-07-13 19:37:12 +0100

    including commented afternoon exercise

--- src/index.js
+++ src/index.js
@@ -5,6 +5,28 @@
 // - Junior passengers (under 18) get a 20% discount
 // - Senior passengers (over 65) get a 40% discount
 
+
+
+// AFTERNOON EXERCISE
+// const userAge = prompt("What's your age?");
+// const travelDistanceInKm = prompt("How far are you going? Please write it in Kilometres");
+
+// const pricePerTravelledKm = 0.21;
+
+// let ticketFare = travelDistanceInKm * pricePerTravelledKm;
+
+// const juniorDiscount = (ticketFare * 20) / 100;
+// const seniorDiscount = (ticketFare * 40) / 100;
+
+// if(userAge <18){
+//   console.log("As an underage, you get 20% discount, so your ticket will cost: " + "£" + (ticketFare-juniorDiscount).toFixed(2));
+// }   else if (userAge >=65){
+//   console.log("As a senior, you get 40% discount, so your ticket will cost: " + "£" + (ticketFare-seniorDiscount).toFixed(2));
+//    else {console.log("Your ticket will cost: " + "£" + ticketFare);
+// }
+
+
+
 // CHALLENGE DONE!
 
 let journey = prompt(
